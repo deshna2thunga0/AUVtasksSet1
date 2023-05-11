c8e104bf0eca0199088f22f8bf92187c19c89910
deshna2thunga0 <deshna2thunga0@gmail.com>
2023-05-11 20:28:01 +0530

    no new commit

--- task4.sh
+++ task4.sh
@@ -1,1 +1,17 @@
-read num
+read num # reading n
+x=0
+i=0
+while [[ $i -lt $num ]];
+do
+  j=0
+  while [[ $j -lt $i+1 ]];
+  do
+    #echo $(( $x%10)) >> varr
+    echo -n $(($x%10)) #option -n removes default new line
+    ((x=x+1))
+    ((j=j+1))
+  done
+  echo -e "\n" #option -e validates escape sequences
+  ((i=i+1))
+done
+
